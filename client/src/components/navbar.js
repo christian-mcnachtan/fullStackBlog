0a080eea5ef54b4248735798942ab391ecdc4b65
ChristianMcNachtan <christian.mcnachtan@gmail.com>
2024-08-23 19:15:58 -0400

    edited navbar.js

---
+++ client/src/components/navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';  // Assuming you are using react-router for navigation
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/projects">Projects</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
